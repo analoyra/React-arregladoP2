8349622e6dbe0adaef0a29c399697aca4c2892ff
Anabel Elisa Loyra <38407533+analoyra@users.noreply.github.com>
2024-06-30 14:40:23 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "./components/layout/Header";
+import Naveg from "./components/layout/Naveg";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter , Routes , Route} from "react-router-dom";
+
+
+import HomePage from "./pages/HomePage";
+import NosotrosPage from "./pages/NosotrosPage";
+import NovedadesPage from "./pages/NovedadesPage";
+import ServiciosPage from "./pages/ServiciosPage";
+import GaleriaPage from "./pages/GaleriaPage";
+import ContactoPage from './pages/ContactoPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+      <Naveg/>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="nosotros" element={<NosotrosPage/>}/>
+        <Route path="novedades" element={<NovedadesPage/>}/>
+        <Route path="contacto" element={<ContactoPage/>}/>
+        <Route path="servicios" element={<ServiciosPage/>}/>
+        <Route path="galeria" element={<GaleriaPage/>}/>
+      </Routes>
+      </BrowserRouter>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Naveg.js
@@ -0,0 +1,22 @@
+
+import './nav.css';
+import { Link } from "react-router-dom";
+
+
+const Naveg = (props) => {
+    return (
+        <nav> 
+            <div>
+                <ul>
+                    <li><Link to="/"> HOME</Link></li>
+                    <li><Link to="/nosotros"> NOSOTROS</Link></li>
+                    <li><Link to="/galeria"> GALERIA</Link></li>
+                    <li><Link to="/servicios"> SERVICIOS</Link></li>
+                    <li><Link to="/contacto"> CONTACTO</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Naveg;
